ade096e945dd75512139f82673898d2cb61d2742
Michael de Silva <michael@mwdesilva.com>
2014-12-09 17:39:47 +0530

    Bump version & ensure the Encryption module is loaded

--- lib/sagepay_protocol3.rb
+++ lib/sagepay_protocol3.rb
@@ -1,5 +1,6 @@
 require "sagepay_protocol3/version"
+require "sagepay_protocol3/encryption"
 
 module SagepayProtocol3
-  # Your code goes here...
+
 end
--- spec/plugin/encryption_spec.rb
+++ spec/plugin/encryption_spec.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'spec_helper'
-require 'sagepay_protocol3/encryption'
 
 module SagepayProtocol3
 
